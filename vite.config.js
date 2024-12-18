3f09ce09c8b3c1f9b760845f4c5890b0c21e78fb
AnnaBredikhina <ann.bredikhina@gmail.com>
2024-12-18 11:49:12 +0300

    Добавление адреса

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: 'https://AnnaBredikhina.github.io/Weather',
   plugins: [
     vue(),
     vueDevTools(),
